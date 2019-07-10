4d8595a306be32ec932c3f8bb5d46cb719aa8f6f
Kimberly <kamueller2@gmail.com>
2019-07-10 18:24:22 -0400

    added seeds data

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Impossible Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Veggie Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Cheeseburger", false);
