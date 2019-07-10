969581c72f7e65ca75482bbd2cb160a3c37f77d4
Kimberly <kamueller2@gmail.com>
2019-07-10 18:33:23 -0400

    added connection module

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+connection.connect(function(error) {
+    if (error) {
+        console.log("error connecting to: " + error.stack);
+        return;
+    }
+    console.log("connected as id: " + connection.threadId);
+})
+
+module.exports = connection;
