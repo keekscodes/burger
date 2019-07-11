0af686d462cba2baa1afebc52d128db366dbd760
Kimberly <kamueller2@gmail.com>
2019-07-11 17:43:40 -0400

    reorganized code

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-var express = require("express");
-var PORT = process.env.PORT || 3000;
-var app = express();
+var express = require("express")
+var exphbs = require("express-handlebars")
+var routes = require("./controllers/burgers_controller.js");
 
-// Serve static content for the app from the "public" directory in the application directory
+var app = express()
+
+var PORT = process.env.PORT || 3000;
+
+// Serve static content 
 app.use(express.static("public"));
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controllers.js");
-
 app.use(routes);
 
 app.listen(PORT, function() {
