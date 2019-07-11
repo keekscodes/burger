ae7a421c84de259c77264dba66365e81296a7e91
Kimberly <kamueller2@gmail.com>
2019-07-11 16:54:51 -0400

    added main layout and structure

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var PORT = process.env.PORT || 3000;
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+// Serve static content for the app from the "public" directory in the application directory
 app.use(express.static("public"));
 
 // Parse request body as JSON
