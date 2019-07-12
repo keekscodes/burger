d1ad3a36123c6b126d23631f9c4219e8ebdcf95a
Kimberly <kamueller2@gmail.com>
2019-07-12 12:30:29 -0400

    fixing paths

--- config/orm.js
+++ config/orm.js
@@ -1,34 +1,34 @@
 var connection = require("../config/connection.js");
 
 function printQuestionMarks(num) {
-    var arr = [];
+    var array = [];
 
     for (var i = 0; i < num; i++) {
-        arr.push("?");
+        array.push("?");
     }
 
-    return arr.toString();
+    return array.toString();
 }
 
 // Helper function to convert object key/value pairs to SQL syntax
-function objToSql(ob) {
-    var arr = [];
+function objToSql(obj) {
+    var array = [];
 
-    // loop through the keys and push the key/value as a string int arr
-    for (var key in ob) {
-        var value = ob[key];
+    // loop through the keys and push the key/value as a string int array
+    for (var key in obj) {
+        var value = obj[key];
         // check to skip hidden properties
-        if (Object.hasOwnProperty.call(ob, key)) {
+        if (Object.hasOwnProperty.call(obj, key)) {
             // if string with spaces, add quotations 
             if (typeof value === "string" && value.indexOf(" ") >= 0) {
                 value = "'" + value + "'";
             }
-            arr.push(key + "=" + value);
+            array.push(key + "=" + value);
         }
     }
 
-    // translate array of strings to a single comma-separated string
-    return arr.toString();
+    // translate arrayay of strings to a single comma-separated string
+    return array.toString();
 }
 var orm = {
     // selectAll()
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,35 +5,78 @@ var burger = require('../models/burger.js');
 
 // Index redirect
 router.get('/', function(req, res) {
-    res.redirect('/index');
-});
-
-
-// Index page (render all burgers to DOM)
-router.get('/index', function(req, res) {
+    // res.redirect('/index');
     burger.selectAll(function(data) {
-        var hbsObject = { burgers: data };
-        //console.log(hbsObject);
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
         res.render('index', hbsObject);
     });
 });
 
 
-// Create a new burger
-router.post('/burger/create', function(req, res) {
-    burger.insertOne(req.body.burger_name, function() {
-        res.redirect('/index');
+// Index page (render all burgers to DOM)
+// router.get('/index', function(req, res) {
+// });
+
+
+//  Create a new burger
+router.post('/api/burgers', function(req, res) {
+    burger.insertOne(req.body.burger_name, function(data) {
+        res.json(data);
     });
 });
-
 
 // Devour a burger
 router.post('/burger/eat/:id', function(req, res) {
     burger.updateOne(req.params.id, function() {
-        res.redirect('/index');
+        res.redirect('/');
     });
 });
 
 
-// Export routes
+// // Export routes
 module.exports = router;
+
+// ORIGINAL LOGIC
+
+// var express = require("express");
+// var router = express.Router();
+// var burger = require('../models/burger.js');
+
+
+// // Index redirect
+// router.get('/', function(req, res) {
+//     res.redirect('/index');
+// });
+
+
+// // Index page (render all burgers to DOM)
+// router.get('/index', function(req, res) {
+//     burger.selectAll(function(data) {
+//         var hbsObject = { burgers: data };
+//         //console.log(hbsObject);
+//         res.render('index', hbsObject);
+//     });
+// });
+
+
+// // Create a new burger
+// router.post('/burger/create', function(req, res) {
+//     burger.insertOne(req.body.burger_name, function() {
+//         res.redirect('/index');
+//     });
+// });
+
+
+// // Devour a burger
+// router.post('/burger/eat/:id', function(req, res) {
+//     burger.updateOne(req.params.id, function() {
+//         res.redirect('/index');
+//     });
+// });
+
+
+// // Export routes
+// module.exports = router;
