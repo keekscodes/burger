333b39136cd1cd271954d461948d1a75a6556ae5
Kimberly <kamueller2@gmail.com>
2019-07-12 19:14:11 -0400

    updates

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-var express = require("express")
-var methodOverride = require("method-override")
-
-var app = express()
+var express = require("express");
 
 var PORT = process.env.PORT || 3000;
 
-// Serve static content 
-app.use(express.static(process.cwd() + "/public"));
+var app = express()
 
-app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+
+// Serve static content 
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 var exphbs = require("express-handlebars")
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller");
-app.use("/", routes);
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
 
 app.listen(PORT, function() {
     console.log("App now listening at localhost:" + PORT);
